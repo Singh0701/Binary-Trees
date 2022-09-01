0df8ac0abbe6dd5a47ebe812f7bfec253e14b3bd
Devender Singh <58650049+Singh0701@users.noreply.github.com>
2022-09-02 00:21:37 +0530

    Update Ceil from BST.java

--- Ceil from BST.java
+++ Ceil from BST.java
@@ -4,14 +4,18 @@ public class Solution {
     public static int findCeil(TreeNode<Integer> root, int x) {
         int ceil = -1;
         while(root != null) {
+            //If a node is present with value exactly equal to the ceil then return it.
             if(root.data == x) return x;
+            //else, check if key value is greater than current node then move to the right subTree.
             if(root.data < x) 
                 root = root.right;
+            //else, go to the left Sub tree and as the current node's value is less than key, store it's value into the ceil.
             else {
                 ceil = root.data;
                 root = root.left;
             }
         }
+        //return ceil.
         return ceil;
     }
 }
