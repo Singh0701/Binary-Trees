4f2cc99827ca534780d47c618dee95c4a97673fd
Devender Singh <58650049+Singh0701@users.noreply.github.com>
2022-09-08 11:11:13 +0530

    Update Inorder Successor in BST.java

--- Inorder Successor in BST.java
+++ Inorder Successor in BST.java
@@ -30,3 +30,59 @@ class Solution
 
 //Time complexity = O(N) + O(Log(N))
 //Space complexity = O(N)
+
+
+//Aproach 2: While doing the inorder and find value next to the given val and return.
+
+class Solution
+{
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+	public Node inorderSuccessor(Node root,Node x)
+    {
+        Node[] prev = new Node[1];
+        inorder(root, null, prev, x);
+        return prev[0];
+    }
+    
+    public void inorder(Node root, Node prev, Node[] ans, Node x) {
+        if(root == null) return;
+        inorder(root.left, prev, ans, x);
+        if(root.data > x.data) {
+            prev = root;
+            ans[0] = prev;
+            x.data = Integer.MAX_VALUE;
+            return;
+        } 
+        inorder(root.right, root, ans, x);
+    }
+}
+
+//Time complexity = O(N)
+//Space complexity = O(N)
+
+
+
+//Aproach 3: Best Approach, Do a binary search by keeping track of a next possible successor.
+
+
+class Solution
+{
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+	public Node inorderSuccessor(Node root,Node x)
+    {
+        Node successor = null;
+        while(root != null) {
+            if(root.data > x.data) {
+                successor = root;
+                root = root.left;
+            }
+            else {
+                root = root.right;    
+            } 
+        }
+        return successor;
+    }
+}
+
+//Time complexity = O(N)
+//Space complexity = O(N)
